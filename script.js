7e7a8da9ea2a4d9fb8a92d519a3ddcde6a3f529b
Ilurocks <68191097+Ilurocks@users.noreply.github.com>
2026-07-02 22:22:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector(".site-header");
+
+const updateHeader = () => {
+  header.classList.toggle("is-scrolled", window.scrollY > 18);
+};
+
+updateHeader();
+window.addEventListener("scroll", updateHeader, { passive: true });
